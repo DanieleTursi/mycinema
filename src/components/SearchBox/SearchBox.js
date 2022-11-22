6eebdd66e4b5f5fe77accbca5f6d4a724a50461e
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-11-22 12:03:13 +0000

    searchbox implemented

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,4 +1,4 @@
-import {useState,useEffect } from 'react';
+import {useState} from 'react';
 import styled from "styled-components"
 import { FiSearch } from 'react-icons/fi'
 
@@ -6,42 +6,35 @@ const SearchBox = () => {
     const [value, setValue] = useState('');
     const TMDB_KEY = process.env.REACT_APP_TMDB_KEY;
     const URL = 'https://api.themoviedb.org/3/'
-    const lang = '&language=en-US&page=1';
     const params = new URLSearchParams({
       api_key: TMDB_KEY,
     })
-    const [result,setResult] = useState([])
+    const [resultMovie,setResultMovie] = useState([])
+    const [resultSeries,setResultSeries] = useState([])
 
-    useEffect(() => {
+    const handleClick= async (value) => {
       if(value.length > 0){
-        fetch(`${URL}movie/popular?${params}${lang}`).then(
-          response => response.json()
-          .then(responseData=> {
-            setResult([]);
-            let searchQuery= value;
-            // for (const key in responseData){
-            //   let res = responseData[key].name();
-            //   if (res.slice(0,searchQuery.length).indexOf(searchQuery) !== -1){
-            //      setResult((prevResult) => {
-            //       return[ ...prevResult,responseData[key].name]
-            //      })
-            //   }
-            // }
-            console.log(response);
-          }).catch(error=>{
-            console.log(error);
-          })
-        )
-      }else{
-        setResult([]);
+        const resMovie= await fetch(`${URL}search/movie?${params}&language=en-US&query=${value}&page=1`)
+        const dataMovie = await resMovie.json()
+        setResultMovie(dataMovie)
+        const resSeries= await fetch(`${URL}search/tv?${params}&language=en-US&query=${value}&page=1`)
+        const dataSeries = await resSeries.json()
+        setResultSeries(dataSeries)
+        console.log(dataMovie, dataSeries)
+          }
+      else{
+        setResultMovie([]);
+        setResultSeries([]);
       }
-    }, [value])
-
-    
-
+    }
+   
+    const handleSubmit=(event)=>{
+      event.preventDefault()
+      handleClick(value)
+    }
  
   return (
-    <Styledform action="">
+    <Styledform onSubmit={handleSubmit}>
      <input
         type="text"
         id="search"
@@ -49,16 +42,16 @@ const SearchBox = () => {
         onChange={(event)=> setValue(event.target.value)}
         value={value}
       />
-   <button type="submit" >
+   <button type="submit">
 		<span><FiSearch/></span>
 	</button>
-  <div className='box'>{result.map((result,index) => (
+  {/* <div className='box'>{result.map((result,index) => (
     <a href="#" key= {index}>
       <div>
         {result}
       </div>
     </a>))}
-  </div>
+  </div> */}
   </Styledform>
   )
 }
