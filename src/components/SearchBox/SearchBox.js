b0a83de328171f3657a25b31c791f73b307556ca
shuyin21 <shuyindev@gmail.com>
2022-11-22 12:06:53 +0000

    nothing

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,78 +1,69 @@
-import {useState,useEffect } from 'react';
+import { useState } from 'react';
 import styled from "styled-components"
 import { FiSearch } from 'react-icons/fi'
 
 const SearchBox = () => {
-    const [value, setValue] = useState('');
-    const TMDB_KEY = process.env.REACT_APP_TMDB_KEY;
-    const URL = 'https://api.themoviedb.org/3/'
-    const lang = '&language=en-US&page=1';
-    const params = new URLSearchParams({
-      api_key: TMDB_KEY,
-    })
-    const [result,setResult] = useState([])
+  const [value, setValue] = useState('');
+  const TMDB_KEY = process.env.REACT_APP_TMDB_KEY;
+  const URL = 'https://api.themoviedb.org/3/'
+  const params = new URLSearchParams({
+    api_key: TMDB_KEY,
+  })
+  const [resultMovie, setResultMovie] = useState([])
+  const [resultSeries, setResultSeries] = useState([])
 
-    useEffect(() => {
-      if(value.length > 0){
-        fetch(`${URL}movie/popular?${params}${lang}`).then(
-          response => response.json()
-          .then(responseData=> {
-            setResult([]);
-            let searchQuery= value;
-            // for (const key in responseData){
-            //   let res = responseData[key].name();
-            //   if (res.slice(0,searchQuery.length).indexOf(searchQuery) !== -1){
-            //      setResult((prevResult) => {
-            //       return[ ...prevResult,responseData[key].name]
-            //      })
-            //   }
-            // }
-            console.log(response);
-          }).catch(error=>{
-            console.log(error);
-          })
-        )
-      }else{
-        setResult([]);
-      }
-    }, [value])
+  const handleClick = async (value) => {
+    if (value.length > 0) {
+      const resMovie = await fetch(`${URL}search/movie?${params}&language=en-US&query=${value}&page=1`)
+      const dataMovie = await resMovie.json()
+      setResultMovie(dataMovie)
+      const resSeries = await fetch(`${URL}search/tv?${params}&language=en-US&query=${value}&page=1`)
+      const dataSeries = await resSeries.json()
+      setResultSeries(dataSeries)
+      console.log(dataMovie, dataSeries)
+    }
+    else {
+      setResultMovie([]);
+      setResultSeries([]);
+    }
+  }
 
-    
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    handleClick(value)
+  }
 
- 
   return (
-    <Styledform action="">
-     <input
+    <Styledform onSubmit={handleSubmit}>
+      <input
         type="text"
         id="search"
         name="search"
-        onChange={(event)=> setValue(event.target.value)}
+        onChange={(event) => setValue(event.target.value)}
         value={value}
       />
-   <button type="submit" >
-		<span><FiSearch/></span>
-	</button>
-  <div className='box'>{result.map((result,index) => (
+      <button type="submit">
+        <span><FiSearch /></span>
+      </button>
+      {/* <div className='box'>{result.map((result,index) => (
     <a href="#" key= {index}>
       <div>
         {result}
       </div>
     </a>))}
-  </div>
-  </Styledform>
+  </div> */}
+    </Styledform>
   )
 }
 
 export default SearchBox
 
-const Styledform=styled.form`
+const Styledform = styled.form`
 text-align:center;
 border-radius:7px;
-
 .box{
   color:black;
 }
-
 input{
     height:30px;
     width:200px;
@@ -81,12 +72,10 @@ input{
     font-family: 'PT Sans Narrow', sans-serif;
     font-size:18px;
 }
-
 input:hover{
     background:lightgray;
     
 }
-
 button{
     height:39px;
     border-radius:0 7px 7px 0;
@@ -97,12 +86,8 @@ button{
     font-family: 'PT Sans Narrow', sans-serif;
     font-size:18px;
 }
-
 button:hover{
     transform:scale(1.03);
 }
 `
-
-
-
 
