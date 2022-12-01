f792782e8a375ad1991e4176a5a26573764ff7db
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-12-01 18:25:58 +0000

    code cleaned

--- src/components/CardWrapper/CardPeople.js
+++ src/components/CardWrapper/CardPeople.js
@@ -5,22 +5,16 @@ import { useNavigate, useParams } from 'react-router-dom';
 import useLocalStorage from '../../hooks/useLocalStorage';
 import noImage from '../../assets/images/noImage.png'
 const CardPeople = (props) => {
-
   const { getActorDetails, details, detailsLoading } = useContext(TmdbContext);
-
   const [actorId, setActorId] = useLocalStorage('actorId', '');
-
-
   const navigate = useNavigate();
+
   const getId = async () => {
-
     await getActorDetails(actorId);
-
   }
 
   const idHandler = async () => {
     await setActorId(props.id);
-
     await getId()
     navigate('/actordetails/')
   }
@@ -45,14 +39,12 @@ const CardPeople = (props) => {
 
 export default CardPeople
 
-
 const ContainerPeople = styled.div`
   display:flex;
   width: 200px;
   height: 280px;
   border-radius: 10px;
   margin-left: 10px;
-//   background-image:url(${noImage});
   background-image:${props => props.bg == null ? `url(${noImage})` : `url(https://www.themoviedb.org/t/p/original${props.bg})`};
   background-size: cover;
   background-repeat: no-repeat;
@@ -69,15 +61,18 @@ const Info = styled.div`
     display:flex;
     justify-content:center;
     align-items:center;
-flex-direction:column;
+    flex-direction:column;
+
     h2{
       margin:4px 0 0 4px;
       text-align:center;
       font-size:14px;
     }
+
     h6{
       margin:0;
     }
+    
     span{
       font-size:8px ;
     }
